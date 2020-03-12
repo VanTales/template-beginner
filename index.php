b815d847ff842e3aec7d37f4da64631623384c82
maricon-fullstack <maricon@fullstack.ph>
2020-03-12 23:34:40 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!Doctype>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<meta name="format-detection" content="telephone=no">
+		<link rel="icon" href="assets/images/favicon.io">
+
+		<title>Template</title>
+
+		<link rel="stylesheet" type="text/css" href="./styles/vendors/bootstrap/bootstrap.min.css">
+	<head>
+	<body>
+		<main class="page-class">
+			<div class="section-page">
+				<div class="container">
+					<div class="content-holder">
+						Hello Word
+					</div>
+				</div>
+			</div>
+		</main>
+
+		<script src="./scripts/vendor/jquery-3.4.1.min"></script>
+		<script src="./scripts/vendor/tether.min.js"></script>
+		<script src="./scripts/vendor/bootstrap/bootstrap.min.js"></script>
+		<script src="./scripts/vendor/bootstrapper.js"></script>
+	</body>
+</html>
